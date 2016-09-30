b31e12291f4be6001f36cc3bd5cfe175a5ef439a
MAG <michailanatger@mail.ru>
2016-09-30 18:47:21 +0500

    Create Class for choose language on first start programm

---
+++ Gripari/LanguageSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hucksters.Gripari
+{
+    class LanguageSwitch
+    {
+        public static void questionLanguage()
+        {
+            Console.WriteLine("Choose your language, write English or Russian,E or R");
+            string language = Console.ReadLine().ToString();
+
+            if (language == "English" || language == "En" || language == "en" || language == "E")
+            {
+
+            }
+
+            if (language == "Russian" || language == "Ru" || language == "ru" || language == "R")
+            {
+
+            }
+            Console.WriteLine(language);
+        }
+
+    }
+}
