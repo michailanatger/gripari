f5d5cf2a412ffb3945a61a1de4e27ec22c29ad3a
MAG <michailanatger@mail.ru>
2016-10-04 14:47:45 +0500

    create virus, with forms application will simply

--- Gripari/Program.cs
+++ Gripari/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
 using Hucksters.Gripari.Input;
 using Hucksters.Gripari.Output;
 
@@ -11,17 +11,20 @@ namespace Hucksters.Gripari
 {
     class Program
     {
+        public static ClickOnceUpdate.Updater.SilentUpdater SilentUpdater { get; set; }
         static void Main(string[] args)
         {
             LanguageSwitch.questionLanguage();
             int number = 1;
             Console.WriteLine(number);
-
+            //SilentUpdater = new ClickOnceUpdate.Updater.SilentUpdater();
+            //SilentUpdater.Restart();
+            //SilentUpdater.ProgressChanged += SilentUpdaterOnProgressChanged;
             var input1c = new OneC();
             var outputWeb = new WebOut();
 
             input1c.GotEventLog += outputWeb.OnEventLog;
-
+            
             input1c.run();
         }
     }
--- Gripari/SilentUpdate.cs
+++ Gripari/SilentUpdate.cs
@@ -3,16 +3,25 @@ using System.ComponentModel;
 using System.Deployment.Application;
 using System.Diagnostics;
 using System.Timers;
+using System.Windows;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
-namespace Hucksters.Gripari
+namespace ClickOnceUpdate.Updater
 {
-    class SilentUpdate : INotifyPropertyChanged
+    public class SilentUpdater : INotifyPropertyChanged
     {
+        private Mutex instanceMutex;
         private readonly ApplicationDeployment applicationDeployment;
-        private readonly Timer timer = new Timer(60000);
+        private readonly System.Timers.Timer timer = new System.Timers.Timer(60000);
         private bool processing;
 
-
+        //public event EventHandler<UpdateProgressChangedEventArgs> ProgressChanged;
         public event EventHandler<EventArgs> Completed;
         public event PropertyChangedEventHandler PropertyChanged;
         private bool updateAvailable;
@@ -34,15 +43,20 @@ namespace Hucksters.Gripari
             if (handler != null) handler(this, null);
         }
 
-
-
-        public SilentUpdate()
+        /*private void OnProgressChanged(UpdateProgressChangedEventArgs e)
         {
+            var handler = ProgressChanged;
+            if (handler != null) handler(this, e);
+        }*/
+
+        public SilentUpdater()
+        {
+
             if (!ApplicationDeployment.IsNetworkDeployed)
                 return;
             applicationDeployment = ApplicationDeployment.CurrentDeployment;
             applicationDeployment.UpdateCompleted += UpdateCompleted;
-
+            //applicationDeployment.UpdateProgressChanged += UpdateProgressChanged;
             timer.Elapsed += (sender, args) =>
             {
                 if (processing)
@@ -57,14 +71,17 @@ namespace Hucksters.Gripari
                 }
                 catch (Exception ex)
                 {
-                    Debug.Write("Check for update failed. " + ex.Message);
+                    Debug.WriteLine("Check for update failed. " + ex.Message);
                     processing = false;
                 }
             };
             timer.Start();
         }
 
-
+        /*void UpdateProgressChanged(object sender, DeploymentProgressChangedEventArgs e)
+        {
+            OnProgressChanged(new UpdateProgressChangedEventArgs(e));
+        }*/
 
         void UpdateCompleted(object sender, AsyncCompletedEventArgs e)
         {
@@ -76,6 +93,54 @@ namespace Hucksters.Gripari
             }
             UpdateAvailable = true;
             OnCompleted();
+        }
+
+        public void Restart()
+        {
+            //var shortcutFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".appref-ms");
+            //CreateClickOnceShortcut(tmpFile);
+
+            var shortcutFile = GetShortcutPath();
+            var proc = new Process { StartInfo = { FileName = shortcutFile, UseShellExecute = true } };
+
+            ReleaseMutex();
+            proc.Start();
+            
+            //app.Shutdown();
+        }
+
+        public static string GetShortcutPath()
+        {
+            return String.Format(@"{0}\{1}\{2}.appref-ms", Environment.GetFolderPath(Environment.SpecialFolder.Programs), GetPublisher(), GetDeploymentInfo().Name.Replace(".application", ""));
+        }
+
+        private void ReleaseMutex()
+        {
+            if (instanceMutex == null)
+                return;
+            instanceMutex.ReleaseMutex();
+            instanceMutex.Close();
+            instanceMutex = null;
+        }
+        public static string GetPublisher()
+        {
+            XDocument xDocument;
+            using (var memoryStream = new MemoryStream(AppDomain.CurrentDomain.ActivationContext.DeploymentManifestBytes))
+            using (var xmlTextReader = new XmlTextReader(memoryStream))
+                xDocument = XDocument.Load(xmlTextReader);
+
+            if (xDocument.Root == null)
+                return null;
+
+            var description = xDocument.Root.Elements().First(e => e.Name.LocalName == "description");
+            var publisher = description.Attributes().First(a => a.Name.LocalName == "publisher");
+            return publisher.Value;
+        }
+
+        private static ApplicationId GetDeploymentInfo()
+        {
+            var appSecurityInfo = new System.Security.Policy.ApplicationSecurityInfo(AppDomain.CurrentDomain.ActivationContext);
+            return appSecurityInfo.DeploymentId;
         }
     }
 }
